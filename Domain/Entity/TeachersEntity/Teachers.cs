66fdf5916490d9d77d9da0d0bdb31321245af222
ibnefer <alexandrealcime3@gmail.com>
2025-10-31 20:24:29 -0400

    Infrastructure_project

---
+++ Domain/Entity/TeachersEntity/Teachers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity.ProfesorEntity
+{
+    public class Teachers
+    {
+        public int TeacherId { get; set; }
+        public string Name { get; set; }
+        public string dni_id { get; set; }
+    }
+}
