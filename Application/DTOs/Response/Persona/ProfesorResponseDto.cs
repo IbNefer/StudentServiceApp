be407144e3a262dbdf19870207ff71801c4fe77d
ibnefer <alexandrealcime3@gmail.com>
2025-11-20 21:06:08 -0500

    features: dto-request

---
+++ Application/DTOs/Response/Persona/ProfesorResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using Application.DTOs.Request.Persona;
+
+namespace Application.DTOs.Response.Persona
+{
+    public class ProfesorResponseDto: CreateProfesorRequestDTO
+    {
+        public int Id { get; set; }
+        public ICollection<ProfesorResponseDto>? Profesores { get; set; }
+    }
+}
